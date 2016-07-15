81007912605c0ee17495dfb618905cebe64f6fde
Heos <heos@ua.pt>
2016-07-15 11:04:04 +0100

    finish

--- src/test/java/com/tmgr/rest/EndPointTest.java
+++ src/test/java/com/tmgr/rest/EndPointTest.java
@@ -29,6 +29,14 @@ public class EndPointTest {
 	public void afterEachTest() {
 		System.out.println("This is exceuted after each Test");
 	}
+        
+        @Test
+	public void testregister(){
+		Response result = service.register("randomusername", "taskid", "abc", "abc", "abc");
+
+		assertEquals(200, result.getStatus());
+		
+	}
 
 	@Test
 	public void testLogin() {
@@ -60,10 +68,36 @@ public class EndPointTest {
 		assertEquals(200, result.getStatus());
 		
 	}
+        
+        
+        
+        @Test
+	public void testmarkcomplete(){
+		Response result = service.markcomplete("randomusername", "47");
+
+		assertEquals(200, result.getStatus());
+		
+	}
 	
 	@Test
-	public void testregister(){
-		Response result = service.register("randomusername", "taskid", "abc", "abc", "abc");
+	public void testdeletetask(){
+		Response result = service.deletetask("randomusername", "47");
+
+		assertEquals(200, result.getStatus());
+		
+	}
+        
+        @Test
+	public void testsavetask(){
+		Response result = service.savetask("randomusername", "randomTitle", "randomMessage", "03/08/2014", "High");
+
+		assertEquals(200, result.getStatus());
+		
+	}
+        
+        @Test
+	public void testupdatetask(){
+		Response result = service.updatetask("47", "randomusername", "randomTitle", "randomMessage", "03/08/2014", "High", "0");
 
 		assertEquals(200, result.getStatus());
 		
